a69f520687f33cb32f3be41ca7d0088f65cf2c35
David Cano <dcanoea@fpvirtualaragon.es>
2025-05-21 22:14:26 +0200

    video5

---
+++ PrimerasFunciones.py
@@ -0,0 +1,12 @@
+def mensaje():#declaracion de la función
+
+    print("Estamos aprendiendo Python")
+    print("Poco a poco")                    ##CUERPO DE LA FUNCIÓN
+    print("Seguiremos aprendiendo")
+    print()
+
+mensaje()#llamada a la función
+mensaje()
+
+print("Ejecutando código fuera de función")
+mensaje()
