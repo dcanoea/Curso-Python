07b0929f71764b1226de9b84301a295fcdd2d4c1
David Cano <dcanoea@fpvirtualaragon.es>
2025-05-22 13:42:52 +0200

    video6

--- PrimerasFunciones.py
+++ PrimerasFunciones.py
@@ -10,3 +10,20 @@ mensaje()
 
 print("Ejecutando código fuera de función")
 mensaje()
+
+
+def suma(num1, num2): #Función con parámetros
+    print(num1+num2)
+
+suma(1,2)#pasamos los parámetros
+suma(111,222)
+
+
+def sumaReturn(num1,num2): #Función que devuelve un resultado (return)
+    resultado=num1+num2
+    return resultado
+
+print(sumaReturn(123,1))
+
+almacena_resultado = sumaReturn(5,8) #Almacenamos en una variable el return de la funcion sumaReturn
+print(almacena_resultado)
