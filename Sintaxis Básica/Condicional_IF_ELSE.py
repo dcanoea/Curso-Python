be10a1446a929a0ea9f96014905037af6da2dbf4
David Cano <dcanoea@fpvirtualaragon.es>
2025-05-27 19:13:03 +0200

    Condicionales II. Video 11

---
+++ Sintaxis Básica/Condicional_IF_ELSE.py
@@ -0,0 +1,10 @@
+print("Verificacion de acceso")
+
+edadUsuario=int(input("Introduce tu edad: "))
+
+if edadUsuario<18:
+    print("No puedes pasar")
+else:
+    print("Puedes pasar")
+
+print("El programa ha finalizado")
